45e27bed8364799d8cbbd7abeb384d2ab60ab6f4
crystalsedge <viindubs@gmail.com>
2022-12-18 13:26:31 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'file-explorer-local',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
